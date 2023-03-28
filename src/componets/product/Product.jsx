37d437560d4e35f52c06a9248d1220e8f7c6229b
Al-habib <alhabib5565@gmail.com>
2023-03-28 15:31:06 +0600

    add cart

--- src/componets/product/Product.jsx
+++ src/componets/product/Product.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 const Product = (props) => {
-    console.log(props)
-    let {img, name, price, quantity} = props.product
+    // console.log(props)
+    let {img, name, price, quantity,seller, ratings} = props.product
+    
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className="details">
             <h6>{name}</h6>
             <p>Price: {price}</p>
+            <p className='mnfctrr'>Manufacturer: {seller}</p>
+            <small>ratings: {ratings}</small>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={()=>props.handelId(props.product)} className='btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
--- src/componets/shop/Shop.jsx
+++ src/componets/shop/Shop.jsx
@@ -3,11 +3,17 @@ import Product from '../product/Product';
 import './Shop.css'
 const Shop = () => {
     let [products, setProducts] = useState([])
+    let [cart, setCart] = useState([])
     useEffect(()=> {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+    function handelId (product){
+        // console.log('added item',product, cart)
+        let newCart = [...cart, product]
+        setCart(newCart)
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -15,11 +21,13 @@ const Shop = () => {
                     products.map(product => <Product 
                         key={product.id} 
                         product={product}
+                        handelId={handelId}
                         ></Product>)
                 }
             </div>
             <div>
                 <h2>This is Order summary</h2>
+                <h3>Order summary : {cart.length}</h3>
             </div>
         </div>
     );
